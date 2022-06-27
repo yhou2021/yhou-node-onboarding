d6643025f079affb1d2c896404d37aebe6ffbf89
Yubing Hou <yubing.hou@infosecinstitute.com>
2022-06-27 15:58:44 -0500

    added endpoints for getting all authors and posts

--- projects/server/src/index.ts
+++ projects/server/src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const port = 3000;
+const prisma = new PrismaClient();
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.get('/authors', async (req, res) => {
+  const allAuthors = await prisma.user.findMany();
+  res.send(allAuthors);
+});
+
+app.get('/posts', async (req, res) => {
+  const allPosts = await prisma.post.findMany();
+  res.send(allPosts);
 });
 
 app.listen(port, () => {
